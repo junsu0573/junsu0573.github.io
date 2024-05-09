6c265c04b6f78dbd62cfb4afbb6f8e3f3b07e046
junsu0573 <junsu0573@naver.com>
2024-05-09 17:29:51 +0900

    Eddit main banner for playinline

--- src/components/MainBanner.js
+++ src/components/MainBanner.js
@@ -2,7 +2,7 @@ import styles from "./MainBanner.module.css";
 function MainBanner() {
   return (
     <div className={styles.Container}>
-      <video muted autoPlay loop>
+      <video muted autoPlay loop playsInline>
         <source src="videos/simpsons_wallpaper.mp4" type="video/mp4" />
       </video>
     </div>
